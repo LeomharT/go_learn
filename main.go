8b903e1290bc8823311f92c8efba6e527ed5a88b
LeomharT <leo.liao.lzy@gmail.com>
2026-08-26 15:30:49 +0800

    feat: basic gin and swagger docs generation

--- main.go
+++ main.go
@@ -1,19 +1,53 @@
+// @title           Swagger Example API
+// @version         1.0
+// @description     This is a sample server celler server.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+// @BasePath  /api/v1
+
+// @securityDefinitions.basic  BasicAuth
+
+// @externalDocs.description  OpenAPI
+// @externalDocs.url          https://swagger.io/resources/open-api/
+
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+
+	_ "go_learn/docs"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func main() {
+	// nam := "123"
+
+	// Create a Gin router with default middleware (logger and recovery)
 	r := gin.Default()
+	// Define a simple GET endpoint
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello World",
+		// Return JSON response
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World!" + "leoliao hello",
 		})
 	})
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
-	r.Run(":3002")
-	fmt.Println("Server running successfully")
+	// Start server on port 8080 (default)
+	// Server will listen on 0.0.0.0:8080 (localhost:8080 on Windows)
+	if err := r.Run(); err != nil {
+		log.Fatalf("failed to run server: %v", err)
+	}
 }
